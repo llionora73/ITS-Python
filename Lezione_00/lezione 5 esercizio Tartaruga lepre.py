9996f924f15130719a60e11e46d3a46309c3e20b
llionora73 <eleonora.desantis@yahoo.it>
2025-04-02 17:33:00 +0200

    new

---
+++ Lezione_00/lezione 5 esercizio Tartaruga lepre.py
@@ -0,0 +1,41 @@
+# gero numeri casuali
+import random
+print(random.randint(1,10))
+
+# genero lista posizioni per ogni tick orologio
+
+posizioni: list = [] 
+for i in range (1, 71):
+    posizioni.append()
+
+# funzione per visualizzare le posizioni sulla corsia di gara
+def gara(T: int, H: int, tick: int)  -> int:
+    if i <= 0
+    return 1
+
+# una funzione per calcolare la mossa della tartaruga
+
+def mossaTartaruga(x: int, tick: int ) -> int:
+    if 1 ≤ i ≤ 5:
+        return  x+3
+    elif 6 ≤ i ≤ 7:
+        return x-6
+    else:
+        return x+1
+
+# una funzione per calcolare la mossa della lepre
+
+def mossaLepre(y: int, tick: int) -> int:
+    if 1 <= i <= 2:
+        return  y+0
+    elif 3 <= i<= 4:
+        return y + 9 
+
+    elif i == 5:
+        return y + 12
+
+    elif 6 <= i <=8
+        return y +1
+
+    else:
+        return x-2
